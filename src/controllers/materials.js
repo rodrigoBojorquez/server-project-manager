6c1eae77a7cf6f89e5e016388399682bff7e2422
rodrigoBojorquez <rbojorquez1620@gmail.com>
2024-01-28 17:13:37 -0500

    se terminaron los endpoints para agregar materiales y para buscarlos

--- src/controllers/materials.js
+++ src/controllers/materials.js
@@ -1,13 +1,83 @@
 import connection from "../database.js"
+import { validationResult } from "express-validator"
 
 
-export const createMaterial = (req, res) => {
+export const createMaterial = async (req, res) => {
+    const errors = validationResult(req)
 
+    if(!errors.isEmpty()) {
+        return res.status(400).json({
+            error: errors.array()
+        })
+    }
+
+    const { materialName, quantity, measure } = req.body
+
+    try {
+        // validate if the material already exist in the db
+        const querySearch = "SELECT * FROM materials WHERE material_name = ?"
+        const [ validation ] = await connection.promise().query(querySearch, [materialName])
+        
+        if(validation.length > 0){
+            return res.status(400).json({
+                error: "the material already exist"
+            })
+        }
+
+        const queryInsert = "INSERT INTO materials (material_name, create_date, quantity, measure) VALUES (?, NOW(), ?, ?)"
+        const [ response ] = await connection.promise().query({sql: queryInsert, values: [materialName, parseInt(quantity), measure]})
+ 
+        return res.json({
+            message: "material added successfully",
+            data: response
+        })
+    }
+    catch (err) {
+        return res.status(500).json({
+            error: err.message
+        })
+    }
 }
 
 
-export const getMaterials = (req, res) => {
+export const getMaterials = async (req, res) => {
+    const errors = validationResult(req)
 
+    if(!errors.isEmpty()) {
+        return res.status(400).json({
+            error: errors.array()
+        })
+    }
+    
+    try {
+        const { page, search } = req.query
+
+        if (search != null) {
+            const querySearch = "SELECT * FROM materials  WHERE material_name LIKE ? ORDER BY id_material DESC LIMIT 10 OFFSET ?"
+            const searchTerm = `%${search}%`
+            const [ results ] = await connection.promise().query(querySearch, [searchTerm, (page - 1) * 15])
+
+            return res.json({
+                message: "successful request",
+                data: results
+            })
+        }
+        else {
+            // search the warehouse 15 by 15
+            const queryPage = "SELECT * FROM materials LIMIT 15 OFFSET ?"
+            const [ results ] = await connection.promise().query(queryPage, [(page - 1) * 15])
+
+            return res.json({
+                message: "successful request",
+                data: results
+            })
+        }
+    }
+    catch (err) {
+        return res.status(500).json({
+            error: err.message
+        })
+    }
 }
 
 
--- src/routes/materialsRoutes.js
+++ src/routes/materialsRoutes.js
@@ -7,13 +7,13 @@ const materialsRouter = express.Router()
 // VALIDATIONS HERE
 const createMaterialChain = [
     body("materialName").trim().isString().isLength({min: 3, max: 255}).withMessage("name must be greater than 3 and lower than 255"),
-    body("quantity").trim().isInt({min: 1}).withMessage("minimun 1 item must be added"),
+    body("quantity").isNumeric().withMessage("minimun 1 item must be added"),
     body("measure").trim().isString().isLength({min: 1, max: 255}).withMessage("invalid unit of measurement")
 ]
 
 const getMaterialsChain = [
-    query("page").isInt({min: 1}).withMessage("invalida page"),
-    query("search").trim().isString().isLength({min:3, max:255}).withMessage("minimun 3 chars to search")    
+    query("page").isInt({min: 1}).withMessage("invalid page"),
+    query("search").trim().isString().isLength({min:3, max:255}).optional().withMessage("minimun 3 chars to search")    
 ]
 
 const updateMaterialChain = [
