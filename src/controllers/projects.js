7cb64049bf4f2e793aeb834d6027228156625501
Israel Boquin <ip345622@gmail.com>
2024-01-31 13:05:24 -0500

    Checando guery innerjon

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,111 +1,129 @@
-import connection from "../database.js"
-import { validationResult } from "express-validator"
-
+import connection from "../database.js";
+import { validationResult } from "express-validator";
 
 // * Create project
 export const createProject = async (req, res) => {
-    const errors = validationResult(req)
+  const errors = validationResult(req);
 
-    if(!errors.isEmpty()) {
-        return res.status(400).json({
-            error: errors.array()
-        })
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      error: errors.array(),
+    });
+  }
+
+  try {
+    const { projectName, projectDescription, materials } = req.body;
+
+    const querySearch = "SELECT * FROM projects WHERE project_name = ?";
+    const [project] = await connection
+      .promise()
+      .query({ sql: querySearch, values: [projectName] });
+    if (project.length != 0) {
+      return res.status(400).json({
+        error: "a project with that name already exists",
+      });
     }
 
-    try {
-        const { projectName, projectDescription, materials } = req.body
+    const queryInsert =
+      "INSERT INTO projects (project_name, project_description, create_date, project_state_fk) VALUES (?, ?, NOW(), ?)";
+    const [result] = await connection.promise().query({
+      sql: queryInsert,
+      values: [projectName, projectDescription, 1],
+    });
 
-        const querySearch = "SELECT * FROM projects WHERE project_name = ?"
-        const [ project ] =  await connection.promise().query({sql: querySearch, values: [projectName]})
-        if (project.length != 0) {
-            return res.status(400).json({
-                error: "a project with that name already exists"
-            })
-        }
+    // validate if the materials quantity is valid
+    if (materials != null) {
+      const querySearch = `
+            SELECT
+    pm.material_fk,
+    m.material_name,
+    m.quantity AS material_quantity,
+    SUM(pm.quantity) AS total_quantity
+FROM
+    project_materials pm
+JOIN
+    materials m ON pm.material_fk = m.id_material
+WHERE
+    pm.material_fk = ?
+GROUP BY
+    pm.material_fk, m.material_name, m.quantity;
+            `;
 
-        const queryInsert = "INSERT INTO projects (project_name, project_description, create_date, project_state_fk) VALUES (?, ?, NOW(), ?)"
-        const [ result ] = await connection.promise().query({sql: queryInsert, values: [projectName, projectDescription, 1]})
-
-        // validate if the materials quantity is valid
-        if (materials != null) {
-            const querySearch = `
-                SELECT
-                    pm.material_fk,
-                    m.material_name,
-                    m.quantity AS material_quantity,
-                    SUM(pm.quantity) AS total_quantity
-                FROM
-                    project_materials pm
-                JOIN
-                    materials m ON pm.material_fk = m.id_material
-                WHERE
-                    pm.material_fk = ?
-                GROUP BY
-                    pm.material_fk, m.material_name, m.quantity;
-            `
-
-            const queryInsertMaterials = `
+      const queryInsertMaterials = `
                 INSERT INTO project_materials 
                 (quantity, project_fk, material_fk)
                 VALUES 
                 (?, ?, ?)
-            `
+            `;
 
-            let formattedMaterialsSum = []
+      let formattedMaterialsSum = [];
 
-            for (const material of materials) {
-                const [ results ] = await connection.promise().query(querySearch, [parseInt(material.id)])
+      for (const material of materials) {
+        const [results] = await connection
+          .promise()
+          .query(querySearch, [parseInt(material.id)]);
 
-                let formatedObj = {
-                    id: parseInt(material.id),
-                    material_name: results.length > 0 ? results[0].material_name : null,
-                    material_quantity: results.length > 0 ? results[0].material_quantity : null,
-                    total_used: results.length > 0 ? results[0].total_quantity : 0,
-                    to_add: material.quantity
-                }
-
-                if (( formatedObj.total_used + formatedObj.to_add ) >  formatedObj.material_quantity) {
-                    return res.status(400).json({
-                        error: "material limit exceeded"
-                    })
-                }
-                
-                // add the results to the arr
-                formattedMaterialsSum.push(formatedObj)
-                const insertedId = result.insertId
-                const [ setMaterial ] = await connection.promise().query(queryInsertMaterials, [formatedObj.to_add, insertedId, formatedObj.id])
-            }
-        }
-
-
+        let formatedObj = {
+          id: parseInt(material.id),
+          material_name: results.length > 0 ? results[0].material_name : null,
+          material_quantity:
+            results.length > 0 ? results[0].material_quantity : null,
+          total_used: results.length > 0 ? results[0].total_quantity : 0,
+          to_add: material.quantity,
+        };
 
         return res.json({
-            message: "team added successfully",
-            data: result
-        })
+          mess: formatedObj,
+        });
+
+        if (
+          formatedObj.total_used + formatedObj.to_add >
+          formatedObj.quantity
+        ) {
+          return res.status(400).json({
+            error: "material limit exceeded",
+          });
+        }
+
+        // add the results to the arr
+        formattedMaterialsSum.push(formatedObj);
+        const insertedId = result.insertId;
+        const [setMaterial] = await connection
+          .promise()
+          .query(queryInsertMaterials, [
+            formatedObj.to_add,
+            insertedId,
+            formatedObj.id,
+          ]);
+      }
     }
-    catch (err) {
-        return res.status(500).json({
-            error: err.message
-        })
-    }
-}
+
+    return res.json({
+      message: "team added successfully",
+      data: result,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message,
+    });
+  }
+};
 
 // * Get project
 export const getProjects = async (req, res) => {
-    const errors = validationResult(req)
+  const errors = validationResult(req);
 
-    if(!errors.isEmpty()) {
-        return res.status(400).json({
-            error: errors.array()
-        })
-    }
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      error: errors.array(),
+    });
+  }
 
-    try {
-        const { page, search } = req.query
+  try {
+    const { page, search } = req.query;
 
-        if (search != null) {
-            const querySearch = `
+    if (search != null) {
+      const querySearch = `
                 SELECT 
                     projects.id_project, 
                     projects.project_name, 
@@ -124,24 +142,25 @@ export const getProjects = async (req, res) => {
                 ORDER BY 
                     projects.id_project DESC 
                 LIMIT 10 OFFSET ?
-            `
-            const searchTerm = `%${search}%`
-            const [ results ] = await connection.promise().query(querySearch, [searchTerm, (page - 1) * 15])
+            `;
+      const searchTerm = `%${search}%`;
+      const [results] = await connection
+        .promise()
+        .query(querySearch, [searchTerm, (page - 1) * 15]);
 
-            if (results.length == 0) {
-                return res.json({
-                    message: "there's no projects found"
-                })
-            }
+      if (results.length == 0) {
+        return res.json({
+          message: "there's no projects found",
+        });
+      }
 
-            return res.json({
-                message: "successful request",
-                data: results
-            })
-        }
-        else {
-            // search the projects 15 by 15
-            const queryPage = `
+      return res.json({
+        message: "successful request",
+        data: results,
+      });
+    } else {
+      // search the projects 15 by 15
+      const queryPage = `
                 SELECT 
                     projects.id_project, 
                     projects.project_name, 
@@ -155,89 +174,100 @@ export const getProjects = async (req, res) => {
                     INNER JOIN project_states ON projects.project_state_fk = project_states.id_project_state 
                     LEFT JOIN users ON projects.id_project = users.team_fk
                         AND users.rol_fk = 2
-                LIMIT 15 OFFSET ?`
-            const [ results ] = await connection.promise().query(queryPage, [(page - 1) * 15])
+                LIMIT 15 OFFSET ?`;
+      const [results] = await connection
+        .promise()
+        .query(queryPage, [(page - 1) * 15]);
 
-            if (results.length == 0) {
-                return res.json({
-                    message: "there's no projects found"
-                })
-            }
+      if (results.length == 0) {
+        return res.json({
+          message: "there's no projects found",
+        });
+      }
 
-            return res.json({
-                message: "successful request",
-                data: results
-            })
-        }
+      return res.json({
+        message: "successful request",
+        data: results,
+      });
     }
-    catch (err) {
-        return res.status(500).json({
-            error: err.message
-        })
-    }
-}
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message,
+    });
+  }
+};
 
 // TODO: Terminar dos enpoints
 
 export const updateProject = async (req, res) => {
-    const errors = validationResult(req)
+  const errors = validationResult(req);
 
-    if(!errors.isEmpty()) {
-        return res.status(400).json({
-            error: errors.array()
-        })
-    }
-    const {id_project,project_name, project_description, project_state_fk, materials} = req.body;
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      error: errors.array(),
+    });
+  }
+  const {
+    id_project,
+    project_name,
+    project_description,
+    project_state_fk,
+    materials,
+  } = req.body;
 
-    try {
+  try {
+    const updateFields = [
+      ...(project_name !== undefined ? ["project_name = ?"] : []),
+      ...(project_description !== undefined ? ["project_description = ?"] : []),
+      ...(project_state_fk !== undefined ? ["project_state_fk = ?"] : []),
+      ...(materials !== undefined ? ["materials = ?"] : []),
+    ];
+    const updateParams = [
+      ...(project_name !== undefined ? [project_name] : []),
+      ...(project_description !== undefined ? [project_description] : []),
+      ...(project_state_fk !== undefined ? [project_state_fk] : []),
+      ...(materials !== undefined ? [materials] : []),
+      id_project,
+    ];
 
-        const updateFields = [
-            ...(project_name !== undefined ? ['project_name = ?'] : []),
-            ...(project_description !== undefined ? ['project_description = ?'] : []),
-            ...(project_state_fk !== undefined ? ['project_state_fk = ?'] : []),
-            ...(materials !== undefined ? ['materials = ?'] : []),
-        ];
-        const updateParams = [
-            ...(project_name !== undefined ? [project_name]: []),
-            ...(project_description !== undefined ? [project_description]: []),
-            ...(project_state_fk !== undefined ? [project_state_fk]: []),
-            ...(materials !== undefined ? [materials]: []),
-            id_project
-        ];
+    const queryUpdateProject = `UPDATE projects SET ${updateFields.join(
+      ", "
+    )} WHERE id_project = ?`;
+    const [updatedProject] = await connection
+      .promise()
+      .query(queryUpdateProject, updateParams);
 
-        const queryUpdateProject = `UPDATE projects SET ${updateFields.join(', ')} WHERE id_project = ?`;
-        const [updatedProject] = await connection.promise().query(queryUpdateProject, updateParams);
-        
-        
-        return res.status(200).json({message: `Project updated successfully. Rows affected ${updatedProject.affectedRows}`});
-        
-    }
-    catch (err) {
-        return res.status(500).json({
-            error: err.message
-        })
-    }
-}
-
+    return res.status(200).json({
+      message: `Project updated successfully. Rows affected ${updatedProject.affectedRows}`,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message,
+    });
+  }
+};
 
 export const deleteProject = async (req, res) => {
-    const errors = validationResult(req)
+  const errors = validationResult(req);
 
-    if(!errors.isEmpty()) {
-        return res.status(400).json({
-            error: errors.array()
-        })
-    }
-    const { id_project } = req.body;
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      error: errors.array(),
+    });
+  }
+  const { id_project } = req.body;
 
-try {
-    const queryDelete = 'DELETE FROM projects WHERE id_project = ?';
-    const [deleted] = await connection.promise().query(queryDelete, [id_project]);
+  try {
+    const queryDelete = "DELETE FROM projects WHERE id_project = ?";
+    const [deleted] = await connection
+      .promise()
+      .query(queryDelete, [id_project]);
 
-    return res.status(200).json({ message: `Project deleted successfully. Rows affected: ${deleted.affectedRows}` });
-} catch (error) {
+    return res.status(200).json({
+      message: `Project deleted successfully. Rows affected: ${deleted.affectedRows}`,
+    });
+  } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Internal Server Error' });
-}
-
-}
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
