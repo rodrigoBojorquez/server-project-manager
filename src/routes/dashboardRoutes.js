0b4dfd22d9974809513fd6beddfab759dfb6bbcb
rodrigo_bojorquez <rbojorquez1620@gmail.com>
2024-02-01 08:41:23 -0500

    validacion de token

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { getPieChartData, getLastMaterialsAdded, getPendingProjects, getCountUsersRol } from "../controllers/dashboard.js"
+import { verifyToken } from "../middleware/validateToken.js"
+import {  assignPermissions } from "../middleware/assignPermissions.js"
 
 const dashboardRouter = express.Router()
 
-dashboardRouter.get("/dashboard/pie-chart", getPieChartData)
-dashboardRouter.get("/dashboard/last-materials", getLastMaterialsAdded)
-dashboardRouter.get("/dashboard/pending", getPendingProjects)
-dashboardRouter.get("/dashboard/count-users", getCountUsersRol)
+dashboardRouter.get("/dashboard/pie-chart", verifyToken, assignPermissions(["administrator"]), getPieChartData)
+dashboardRouter.get("/dashboard/last-materials",verifyToken, assignPermissions(["administrator"]), getLastMaterialsAdded)
+dashboardRouter.get("/dashboard/pending", verifyToken, assignPermissions(["administrator"]),getPendingProjects)
+dashboardRouter.get("/dashboard/count-users", verifyToken, assignPermissions(["administrator"]),getCountUsersRol)
 
 export default dashboardRouter
