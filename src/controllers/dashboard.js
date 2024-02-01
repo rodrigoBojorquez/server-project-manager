20f0729f909d40d30aa6dbe6aceb94d98108ea62
rodrigo_bojorquez <rbojorquez1620@gmail.com>
2024-02-01 08:09:40 -0500

    se terminaron los endpoints de dash y se agregaron los validaciiones de acceso

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -70,45 +70,51 @@ export const getPendingProjects = async (req, res) => {
 
 
 export const getLastMaterialsAdded = async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            error: errors.array()
-        });
-    }
-
-    try {
-        const query = "SELECT * FROM materials ORDER BY create_date DESC LIMIT 10";
-        const [ materials ] = await connection.promise().query(query);
-
-        return res.status(200).json({
-            message: "successfull request",
-            data: materials[0]
-        });
-    } catch (err) {
-        return res.status(500).json({
-            error: err.message
-        });
-    }
-};
-
-
-export const getCountUsersRol = async (req, res) => {
     const errors = validationResult(req)
 
-    if(!errors.isEmpty()) {
+    if (!errors.isEmpty()) {
         return res.status(400).json({
             error: errors.array()
         })
     }
 
     try {
+        const query = "SELECT * FROM materials ORDER BY create_date DESC LIMIT 5";
+        const [ materials ] = await connection.promise().query(query)
 
-    }
-    catch (err) {
+        return res.status(200).json({
+            message: "successfull request",
+            data: materials
+        })
+    } catch (err) {
         return res.status(500).json({
             error: err.message
         })
     }
 }
+
+
+export const getCountUsersRol = async (req, res) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            error: errors.array()
+        })
+    }
+
+    try {
+        const query = "SELECT title, COUNT(*) as userCount FROM rols INNER JOIN users ON rols.id_rol = users.rol_fk GROUP BY title"
+        
+        const [ userCountsByRol ] = await connection.promise().query(query)
+
+        return res.status(200).json({
+            message: "successfull request",
+            data: userCountsByRol
+        })
+    } catch (err) {
+        return res.status(500).json({
+            error: err.message
+        })
+    }
+};
