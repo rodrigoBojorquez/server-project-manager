231e481abdd70d832b896c503cb16317a0f6ee59
rodrigo_bojorquez <rbojorquez1620@gmail.com>
2024-02-04 09:11:18 -0500

    se corrigio el back

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -7,17 +7,33 @@ dotenv.config();
 export const getEmployees = async (req, res) => {
   
     try {
-  
+      const {rol, page, search} = req.query
       // Obtener empleados con el rol específico
-      const queryEmployees = "SELECT * FROM users WHERE;";
-      const [employeesSearch] = await connection.promise().query(queryEmployees);
-  
+      if (rol) {
+        const queryEmployees = "SELECT * FROM users WHERE rol_fk = ? LIMIT 10 OFFSET ?;";
+        const [employeesSearch] = await connection.promise().query(queryEmployees, rol, (page - 1) * 10);
+        return res.json({
+          data: employeesSearch
+        })
+      }
+
+      if (search) {
+        const searchTerm = `%${search}%`
+        const queryEmployees = "SELECT * FROM users WHERE username LIKE ? LIMIT 10"
+        const [employeesSearch] = await connection.promise().query(queryEmployees, searchTerm)
+        return res.json({
+          data: employeesSearch
+        })
+      }
+
+      const queryEmployees = "SELECT * FROM users LIMIT 10 OFFSET ?"
+      const [employeesSearch] = await connection.promise().query(queryEmployees, (page - 1) * 10)
       // Devolver la lista de empleados
-      return res.json(employeesSearch);
+      return res.json({
+        data: employeesSearch
+      });
     } catch (error) {
       console.error(error);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   };
-
-
